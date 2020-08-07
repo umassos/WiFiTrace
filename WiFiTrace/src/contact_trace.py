e4823199900f84de627e124cc0a32279ae78ec4c
ameetrivedi <ameetrivedi@gmail.com>
2020-08-07 06:02:20 -0400

    Fix the year check and reverting the file to commit : 08917c86e1183813d13cae72a908e0ff4b508152

--- WiFiTrace/src/contact_trace.py
+++ WiFiTrace/src/contact_trace.py
@@ -3,23 +3,23 @@ BSD 3-Clause License
 
 Copyright (c) 2020, UMass Laboratory for Advanced Systems Software All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
     Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 
-    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following 
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
     disclaimer in the documentation and/or other materials provided with the distribution.
 
-    Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products 
+    Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products
     derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
-STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
@@ -127,16 +127,16 @@ def valid_args(patient_mac, numdays, date):
     month = int(date[4:6])
     date_val = int(date[6:])
 
-    if (year > 2015) and (year < 2022):
-    if (month > 0) and (month < 13):
-        if (date_val > 0) and (date_val < 31):
-            date_flag = True
+    if (year > 2015) and (year < 2025):
+        if (month > 0) and (month < 13):
+            if (date_val > 0) and (date_val < 31):
+                date_flag = True
+            else:
+                print(">> Invalid date of the month \n")
         else:
-            print(">> Invalid date of the month \n")
+            print(">> Invalid month of the year \n")
     else:
-        print(">> Invalid month of the year \n")
-    else:
-       print(">> Year not present in the WiFi log database (must be between 2016 and 2021)\n")
+        print(">> Year not present in the WiFi log database \n")
 
     if (patient_mac_flag and numdays_flag and date_flag):
         return_flag = True
